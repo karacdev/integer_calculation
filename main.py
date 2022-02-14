0e406ba4799e1f44d3712568de49bd96185b868f
karaCDev <72567414+karacdev@users.noreply.github.com>
2022-02-14 17:01:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+integer_division = 13 // 5
+print(integer_division)
+
+
+remainder = 13 % 5
+print(remainder) 
+#when you use the % python divides then calculates whats left over
+#with even numbers the remainder is always 0, with odd the remainder is always 1.
+
+x = 37
+remainders = x % 2
+print(remainders)
